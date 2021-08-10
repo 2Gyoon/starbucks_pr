27df51266536773e83cf4ab5e108510d31b9d05f
2Gyoon <2gyoon52@naver.com>
2021-08-10 22:18:30 +0900

    0810

--- js/script.js
+++ js/script.js
@@ -17,13 +17,34 @@ new Swiper("#notice .top .swiper-container", {
     loop: true
 });
 
+new Swiper("#notice .promotion .swiper-container", {
+    autoplay: {
+        delay: 5000
+    },
+    loop: true,
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 10,
+    pagination: {
+        el: ".promotion .swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        prevEl: ".promotion .swiper-prev",
+        nextEl: ".promotion .swiper-next"
+    }
+    
+})
+
 
 // 프로모션 감추고 보여주기
 const promo_tog = document.querySelector("#notice .right img");
+//버튼
 
 promo_tog.addEventListener("click", function(){
     let active = promo_tog.classList.contains("active");
     const promoPart = document.querySelector("#notice .promotion");
+    //프로모션 슬라이드 파트
 
     if(active){
         promo_tog.classList.remove("active");
