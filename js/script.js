88c771b87ded1204bcd361a31b9dbe7e7783f368
2Gyoon <2gyoon52@naver.com>
2021-08-12 15:59:22 +0900

    0812

--- js/script.js
+++ js/script.js
@@ -34,6 +34,18 @@ new Swiper("#notice .promotion .swiper-container", {
         nextEl: ".promotion .swiper-next"
     }
     
+});
+
+new Swiper("#awards .wrap .swiper-container", {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: "#awards .swiper-prev",
+        nextEl: "#awards .swiper-next",
+    }
+
 })
 
 
@@ -61,6 +73,21 @@ const seasonOffset = document.querySelector("#season").offsetTop;   //2464
 const b_Height = window.innerHeight;
 console.log(seasonOffset)
 
+const reserveLeft = document.querySelector("#reserve .left");
+const reserveText = document.querySelector("#reserve .text");
+const reserveRight = document.querySelector("#reserve .right");
+const reserveOffset = document.querySelector("#reserve").offsetTop; //2864
+
+const pickTop = document.querySelector("#pick_fav .top");
+const pickMid = document.querySelector("#pick_fav .mid");
+const pickBtn = document.querySelector("#pick_fav a");
+const pickOffset = document.querySelector("#pick_fav").offsetTop;
+
+const findLeft = document.querySelector("#find_store .left");
+const findRight = document.querySelector("#find_store .right");
+const findOffset = document.querySelector("#find_store").offsetTop;
+
+
 function scrollEvt(){
     const scroll_top = window.scrollY;
 
@@ -72,8 +99,37 @@ function scrollEvt(){
         seasonRight.classList.remove("active");
     }
 
+    if(scroll_top > reserveOffset - (b_Height / 2 + 500)){
+        reserveLeft.classList.add("active");
+        reserveText.classList.add("active");
+        reserveRight.classList.add("active");
+    }else{
+        reserveLeft.classList.remove("active");
+        reserveText.classList.remove("active");
+        reserveRight.classList.remove("active");
+    }
+
+    if(scroll_top > pickOffset - (b_Height / 2 + 500)){
+        pickTop.classList.add("active");
+        pickMid.classList.add("active");
+        pickBtn.classList.add("active");
+    }else{
+        pickTop.classList.remove("active");
+        pickMid.classList.remove("active");
+        pickBtn.classList.remove("active");
+    }
+
+    if(scroll_top > findOffset - (b_Height / 2 + 550)){
+        findLeft.classList.add("active");
+        findRight.classList.add("active");
+    }else{
+        findLeft.classList.remove("active");
+        findRight.classList.remove("active");
+    }
 }
 
 // body에 onscroll 설정을 안 해놔서 계속 작동이 안됐음..ㅠ 드디어 해결!
+
+
 
 
